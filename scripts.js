9308015ccbf011bf2db4e868a52ffbaf07a0d553
user <user@MacBook-Air-user.local>
2019-10-20 12:55:38 +0300

    add solution

--- scripts.js
+++ scripts.js
@@ -20,14 +20,121 @@ function bubbleSort(arr) {
     return arr;
 }
 
-// function selectSort(arr) {
-//     let min = 0;
-//     for(let i = 0; arr.length; i++) {
-//         if(arr[i])
-//     }
-// }
+ function selectSort(arr) {
+     let resultArr = [];
+     let min = arr[i];
+     for(let i = 0; arr.length; i++) {
+         if(arr[i] < min) {
+            min = arr[i];
+
+         }
+     }
+}
+
+let treeObj = {
+    node: 'A',
+    left: {
+        node: 'B',
+        left: {
+            node: 'C',
+            left: {
+                node: 'E'
+            }
+        },
+        right: {
+            node: 'D'
+        }
+    },
+    right: {
+        node: 'F'
+    }
+}
+
+function preOrder(tree) {
+    let result = [];
+
+    result.push(tree.node)
+    if(tree.left) {
+        checkLeaf(tree.left, result);
+    }
+    if(tree.right) {
+        checkLeaf(tree.right, result);
+    }
+    return result;
+}
+
+function checkLeaf(node, result) {
+    result.push(node.node);
+    if(node.left) {
+        checkLeaf(node.left, result);
+    }
+    if(node.right) {
+        checkLeaf(node.right, result)
+    }
+}
 
 window.bubbleSort = bubbleSort;
 
+var result = [];
 
-console.log(bubbleSort([5, 1, 3, 10, 4, 9]))
+function postOrder(node){
+    let result = [];
+    if (node == null) return;
+    checkLeaf2(node.left, result);
+    checkLeaf2(node.right, result);
+    result.push(node.node);
+    //console.log(node.node);
+
+    return result;
+} 
+
+function checkLeaf2(node, result) {
+    if(node.left) {
+        checkLeaf(node.left, result);
+    }
+    if(node.right) {
+        checkLeaf(node.right, result)
+    }
+    result.push(node.node);
+}
+
+console.log(postOrder(treeObj))
+
+function bracketsFind(str) {
+    const hashBrackets = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    };
+    let stack = [];
+    let result = true;
+    for(let i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case '(':
+            case '{':
+            case '[':
+                stack.push(str[i]);
+                break;
+            default:
+                break;
+        }
+        switch (str[i]) {
+            case ')':
+            case ']':
+            case '}':
+                if(stack.length === 0 || stack[stack.length - 1] !== hashBrackets[str[i]]) {
+                    return result = false;
+                } else {
+                    stack.pop();
+                }
+                break;
+            default:
+                break;
+        }
+        if(!result) return;
+    }
+    return result;
+}
+
+
+console.log(bracketsFind('ava()[{]}'));
